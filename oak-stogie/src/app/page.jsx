e903689f1d5395605537aab6e1fdc99bf7d04e4d
Vini-Nice <viniciussantanapsn@gmail.com>
2025-04-03 16:42:33 -0300

    fiz nada, so para poder dar o pull

--- oak-stogie/src/app/page.jsx
+++ oak-stogie/src/app/page.jsx
@@ -7,6 +7,9 @@ export default function Home() {
     return (
         <>
             <Header />
+            <div className="logo-principal">
+                <img src="" alt="" />
+            </div>
         </>
     );
 }
