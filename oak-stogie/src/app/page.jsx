8f3223fb4e345f756bc3fce1c4051cd559f0fd21
Vini-Nice <viniciussantanapsn@gmail.com>
2025-04-03 16:06:39 -0300

    Mudei de page.js para page.jsx e adicionei a pasta fonts, incluindo a fonte corleone no css global

---
+++ oak-stogie/src/app/page.jsx
@@ -0,0 +1,15 @@
+'use client';
+
+import { useState} from 'react';
+
+export default function Home() {
+  return (
+    <>
+      <header>
+        <nav>
+          <div className="logozinha"></div>
+        </nav>
+      </header>
+    </>
+  )
+}
