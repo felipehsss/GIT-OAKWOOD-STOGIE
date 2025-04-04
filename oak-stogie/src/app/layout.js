cd3291a5ac96ada477201e09ec62514b77fff302
caiofrvx <caioalbuquerque.jcv@gmail.com>
2025-04-04 16:32:51 -0300

    adicionei as questions no layout

--- oak-stogie/src/app/layout.js
+++ oak-stogie/src/app/layout.js
@@ -2,6 +2,7 @@ import Header from '../components/Header/Header';
 import "./globals.css";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Footer from '../components/Footer/Footer';
+import Questions from '../components/Questions/Questions';
 // import '@fortawesome/fontawesome-svg-core/styles.css'
 // import { config } from '@fortawesome/fontawesome-svg-core'
 // config.autoAddCss = false // Evita que o CSS seja adicionado automaticamente
@@ -18,6 +19,8 @@ export default function RootLayout({ children }) {
         
         
         {children}
+
+        <Questions/>
 
         <Footer />
       
