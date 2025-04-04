62d00de817f0a0d85a24c1d03a052f25b803d8a3
felipehsss <felipehsacchi@gmail.com>
2025-04-04 15:57:51 -0300

    arrumei o home principal e o header

--- oak-stogie/src/components/Header/Header.jsx
+++ oak-stogie/src/components/Header/Header.jsx
@@ -3,20 +3,21 @@ import './Header.css';
 
 const Header = () => {
     return (
-        <header className="container text-center header">
+        <header className="container-fluid text-center header">
 
             <div className="row">
-            <div className="col">
+            <div className="col ">
                 <img src="/image-logo/page-2.jpeg"  className='img-fluid' alt="Logo" />
             </div>
             
-            <nav className="col">
-                <a href="/">Home</a>
-                <a href="/about">About</a>
-                <a href="/contact">Contact</a>
+            <nav className="col ">
+                <a className='nav-link' href="/Home">Home</a>
+                <a className='nav-link' href="/about">About</a>
+                <a className='nav-link' href="/contact">Contact</a>
+                <a className='nav-link' href="/produtos">Produtos</a>
             </nav>
 
-            <div className="col">
+            <div className="col ">
                 <a href="/login">Login</a>
                 <a href="/signup">Sign Up</a>
             </div>
--- oak-stogie/test.jsx
+++ oak-stogie/test.jsx
@@ -1,17 +1,22 @@
-<div class="container text-center">
-<div class="row">
-  
-  <div class="col">
-    Column
-  </div>
-  
-  <div class="col">
-    Column
-  </div>
-  
-  <div class="col">
-    Column
-  </div>
+<header className="container-fluid text-center header">
+  <div className="row d-flex align-items-center justify-content-between">
+    {/* Logo */}
+    <div className="col-auto">
+      <img src="/image-logo/page-2.jpeg" className='img-fluid' alt="Logo" />
+    </div>
+    
+    {/* Menu de Navegação */}
+    <nav className="col-auto d-flex">
+      <a className='nav-link' href="/Home">Home</a>
+      <a className='nav-link' href="/about">About</a>
+      <a className='nav-link' href="/contact">Contact</a>
+      <a className='nav-link' href="/produtos">Produtos</a>
+    </nav>
 
-</div>
-</div>
+    {/* Links de Login e Sign Up */}
+    <div className="col-auto">
+      <a className='nav-link' href="/login">Login</a>
+      <a className='nav-link' href="/signup">Sign Up</a>
+    </div>
+  </div>
+</header>
