b41b1f1206b8a4df3566216128e082e70e7199c5
caiofrvx <caioalbuquerque.jcv@gmail.com>
2025-04-11 14:34:52 -0300

    nhe
    area dos melhores charutos

---
+++ oak-stogie/src/components/Stogies/Stogies.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Stogies.css';
+
+
+
+const Stogies = () => {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-9">.col-9</div>
+                <div className="col-4">.col-4
+                Since 9 + 4 = 13  12, this 4-column-wide div gets wrapped onto a new line as one contiguous unit.</div>
+                <div className="col-6">.col-6Subsequent columns continue along the new line.</div>
+            </div>
+        </div>
+    );
+}
